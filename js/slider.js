a73b5f7a329e3889c0340ca2de3ed3e6ac0b7d0c
Jun Ho Park <pjunho@umich.edu>
2019-05-17 22:04:45 -0400

    33

--- js/slider.js
+++ js/slider.js
@@ -37,20 +37,23 @@ var slider = function (sliderElement) {
 	  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 	  var scrolled = (winScroll / height) * 100;
 	  var currentSlide = 1;
-	  if (scrolled < 20) {
+	  if (scrolled < 10) {
 			currentSlide = 1;
 	  }
-	  else if (scrolled >= 20 && scrolled < 42) {
+	  else if (scrolled >= 10 && scrolled < 27) {
 		  currentSlide = 2;
 	  }
-	  else if (scrolled >= 42 && scrolled < 85) {
+	  else if (scrolled >= 27 && scrolled < 55) {
 			currentSlide = 3;
 	  }
-	  else if (scrolled >= 85 && scrolled < 97) {
+	  else if (scrolled >= 55 && scrolled < 90) {
 			currentSlide = 4;
 	  }
-	  else {
+	  else if (scrolled >= 90 && scrolled < 95){
 		  currentSlide = 5;
+	  }
+	  else {
+	  	currentSlide = 6;
 	  }
 	  document.querySelector('a.slider__indicator--active').classList.remove('slider__indicator--active');
 	  document.querySelector('a[data-slider-target-index="' + currentSlide +'"]').classList.add('slider__indicator--active');
