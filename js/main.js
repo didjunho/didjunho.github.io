6c0164224745cc304befbe86e5675a9826eca651
Jun Ho Park <pjunho@umich.edu>
2019-05-19 01:57:34 -0400

    37

--- js/main.js
+++ js/main.js
@@ -459,10 +459,4 @@ $(document).ready(function() {
         }
     }
 
-    //------- Mailchimp js --------//  
-
-    $(document).ready(function() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    });
-
 });
